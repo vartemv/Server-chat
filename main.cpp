e774e793b4d33f52c6c3ad12631aebd901405b3e
artem_vereninov <duboshit50@gmail.com>
2024-04-22 18:03:37 +0200

    test

--- main.cpp
+++ main.cpp
@@ -12,6 +12,7 @@ int main(int argc, char *argv[]) {
     ArgumentsHandler ah{};
     ah.get_args(argc, argv);
 
+
     ThreadPool tp{ah.get_threads()};
     std::stack<UserInfo> s;
     synch synch_variables(0);
